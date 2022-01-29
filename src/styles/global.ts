58fb5f4585a398d1c4c0deb1bf775a46e865a070
unknown <gcolares95@gmail.com>
2022-01-29 15:30:04 -0300

    Componente Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,15 +2,16 @@ import { createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
+    --background: #f0f2f5;
     --red: #E52E4D;
     --blue: #5429CC;
-    
+    --green: #33CC95;
+
     --blue-light: #6933FF;
     
     --text-title: #363F5F;
     --text-body: #969CB3;
     
-    --background: #f0f2f5;
     --shape: #FFFFFF;
   }
   
