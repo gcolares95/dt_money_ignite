1c8cb96914be717723f834196cc32aaada7622d6
unknown <gcolares95@gmail.com>
2022-01-29 14:46:18 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { GlobalStyle } from "./styles/global";
+import { Header } from './components/Header'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <h1>Hello world</h1>
-    </div>
-  );
+      <Header />
+    </>
+  )
 }
