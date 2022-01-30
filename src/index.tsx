ffdc5dd7b30c1adf47078ac4998e1c6e67f4e62e
unknown <gcolares95@gmail.com>
2022-01-30 16:35:07 -0300

    configurando miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+    routes() { // definindo quais rotas vai ter na api ficticia
+      this.namespace = 'api';
+
+      // fazendo requisição get para rota 'transactions', obs: não precisa colocar 'api'
+      this.get('/transactions', () => {
+        return [
+          {
+            id: 1,
+            title: 'transaction 1',
+            amount: 400,
+            type: 'deposit',
+            category: 'Food',
+            createAt: new Date()
+          }
+        ]
+      }) 
+    }
+})
 
 ReactDOM.render(
   <React.StrictMode>
