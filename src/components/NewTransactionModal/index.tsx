ededffa572a4f1353189425b346af7ff4cfb4a58
unknown <gcolares95@gmail.com>
2022-02-01 20:28:11 -0300

    Estrutura do formulário & Estilizando Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,51 @@
 import Modal from 'react-modal';
 
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+
 interface NewTransactionModalProps {
   isOpen: boolean;  // props -> se o Modal está aberto ou não
   onRequestClose: () => void; // começando com 'on', pattern para função recebida em props :p
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay" // para estilizar o modal do 0
+      className="react-modal-content" // className no Model vai ser aplicado ao Content do modal
     >
-      <h2>Cadastrar Transação</h2>
+
+      <button 
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+        >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input 
+          placeholder="Título" 
+        />
+
+        <input 
+          type="number"
+          placeholder="valor"
+        />
+
+       <input 
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar  
+        </button> 
+      </Container>
     </Modal>
+
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -54,4 +54,42 @@ html {
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5); // preto com 50% deopacidade
+    
+    position: fixed; // ajuda pra quando a page tiver scroll
+    top: 0;
+    left: 0; // top, left, right, bottom: 0 -> ou seja, vai ocupar tudo
+    right: 0;
+    bottom: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px; // se tiver menos de 576px width, modal-content vai ocupar 100% da width
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+
 `;
