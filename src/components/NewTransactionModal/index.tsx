bc8275cf86285c446a73246fccaaccc3bfc82348
unknown <gcolares95@gmail.com>
2022-02-02 22:25:19 -0300

    Listando transações e seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outComeImg from '../../assets/outcome.svg';
 import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;  // props -> se o Modal está aberto ou não
@@ -21,12 +22,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) { 
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type
-    })
+    }
+
+    api.post('/transactions', data);
   }
   
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+    // banco de dados interno do mirage
+    models: {
+      transaction: Model,// primeiro modelo
+    },
+
+    // deixandos dados pré-cadastrados p/ deixar a interface mais amigável
+    seeds(server) { 
+      server.db.loadData({
+        transactions: [       // nome da tabela, que é o nome do modelo no plural
+          {
+            id: 1,
+            title: 'Freelancer de website',
+            type: 'deposit',
+            category: 'Dev',
+            amount: 6000,
+            createdAt: new Date('2021-02-12 09:00:00'),
+          },
+          {
+            id: 2,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'casa',
+            amount: 1100,
+            createdAt: new Date('2021-02-14 11:00:00'),
+          },
+        ],
+      })
+    },
+
     routes() { // definindo quais rotas vai ter na api ficticia
       this.namespace = 'api'; // dizendo pro mirage que todas chamadas a api q será feita, vai estar apartir de '/api', será direcionada para o miragejs
 
       // fazendo requisição get para rota 'transactions', obs: não precisa colocar 'api'
       this.get('/transactions', () => {
-        return [
-          {
-            id: 1,
-            title: 'transaction 1',
-            amount: 400,
-            type: 'deposit',
-            category: 'Food',
-            createAt: new Date()
-          }
-        ]
-      }) 
+        return this.schema.all('transaction'); // retornando as transações
+      })
+
+      // criando rota para inserir transactions
+      // request -> dados que estão sendo enviados em api.post('/transactions', data);
+      this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody) // request.requestBody, por padrão os dados vem em texto, como estamos enviando um json (objeto), precisamos converter o dado de texto para objeto no js.
+        
+        // transaction -> model que eu quero inserir, data -> os dados
+        return schema.create('transaciton', data); /// schema -> banco de dados  
+      })
     }
 })
 
